52abb41f04e5308de8f0fa864a819ecd1e30bf00
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-05 03:45:50 -0400

    roll(feat): Add initial rough draft for sampling dice rolls

--- linear/roll.py
+++ linear/roll.py
@@ -1,17 +1,18 @@
 """
 Script: linear.roll
 
-Generate a set of values for generating linear sequences
+Generate a set of values for generating linear sequences with replacement
 
 Generate a distribution set within a given range
 """
 
 from argparse import ArgumentParser, Namespace
+from math import pow
 from random import randrange, seed
 
 
 def dice_roll() -> int:
-    return [1, 2, 3, 4, 5, 6][randrange(1, 6)]
+    return [1, 2, 3, 4, 5, 6][randrange(6)]
 
 
 def get_args() -> Namespace:
@@ -21,13 +22,20 @@ def get_args() -> Namespace:
         "--seed",
         type=int,
         default=42,
-        help="See Douglas Adams. Defaults to 42.",
+        help="Seed for deterministic output. Defaults to 42.",
     )
     parser.add_argument(
         "-d",
         "--deterministic",
         action="store_true",
-        help="Enable deterministic output. Default is False.",
+        help="Enable deterministic output.",
+    )
+    parser.add_argument(
+        "-n",
+        "--sample-size",
+        type=int,
+        default=10,
+        help="Number of dice rolls (sample size).",
     )
     return parser.parse_args()
 
@@ -35,12 +43,23 @@ def get_args() -> Namespace:
 def main():
     # Get user parameters
     args = get_args()
+
+    # Population: Die faces
+    population = [1, 2, 3, 4, 5, 6]
+    population_size = len(population)
+
+    # Total permutations with replacement: j^n
+    total_permutations = int(pow(population_size, args.sample_size))
+    print(f"Total possible permutations with replacement: {total_permutations}")
+
     # Set a seed for deterministic output if the flag is set
     if args.deterministic:
         seed(args.seed)
-    # Create a sample size of 10
-    for i in range(10):
-        print(f"i: {i}, roll: {dice_roll()}")
+
+    # Sample rolls
+    for i in range(args.sample_size):
+        roll = dice_roll()
+        print(f"i: {i + 1}, roll: {roll}")
 
 
 if __name__ == "__main__":
